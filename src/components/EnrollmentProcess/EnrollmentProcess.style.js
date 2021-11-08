c8f0034388eb00b941b856eabb1128ad8c21f3c3
Daryl Thomas <thomastw935@gmail.com>
2021-11-09 04:39:43 +0800

    Added Enrollment Process
    Co-authored-by: Rupert Angelo Duazo <livingglory1@users.noreply.github.com>
    Co-authored-by: Mymy1994 <Mymy1994@users.noreply.github.com>
    Co-authored-by: kevinkei1323 <kevinkei1323@users.noreply.github.com>

--- src/components/EnrollmentProcess/EnrollmentProcess.style.js
+++ src/components/EnrollmentProcess/EnrollmentProcess.style.js
@@ -1,17 +1,35 @@
 import styled from "styled-components";
 
-const Con = styled.div``
-
-const title = styled.h2``
-const icon = styled.div``
-const list = styled.ul`
+const Con = styled.div`
   display:flex;
+  flex-direction: column;
+  align-items:center;
+  gap: 1em;
+`;
+
+const title = styled.h2`
+`;
+const icon = styled.div``;
+const list = styled.ul`
+  display: flex;
   justify-content: space-around;
-`
+`;
+const listItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  div {
+    padding: 1rem;
+    border-radius: 50%;
+    background: #f1f4f7;
+  }
+`;
 
+Con.Title = title;
+Con.Icon = icon;
+Con.List = list;
+Con.ListItem = listItem;
 
-Con.Title = title
-Con.Icon = icon
-Con.List = list
-
-export default Con
+export default Con;
--- src/components/EnrollmentProcess/index.js
+++ src/components/EnrollmentProcess/index.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React from "react";
 import { ClipboardCheckmark } from "@styled-icons/fluentui-system-regular";
 import { Like, UserPlus } from "@styled-icons/boxicons-regular";
-import Con from './EnrollmentProcess.style'
+import Con from "./EnrollmentProcess.style";
+import Arrow1 from "../../assets/images/EnrollmentProcessArrow1.PNG";
+import Arrow2 from "../../assets/images/EnrollmentProcessArrow2.PNG";
 
 export default function index() {
   return (
     <Con>
-        <Con.Title> Request a Tutor via our simple 3-Step Process </Con.Title>
-        <Con.List>
-          <li>
-            <span>Step 1</span>
-            <ClipboardCheckmark width={30} height={30} />
-            <h3>Request</h3>
-            <p>For a suitable tutor Via a 3-min call</p>
-          </li>
-          <li>
-            <span>Step 2</span>
-            <Like width={30} height={30} />
-            <h3>Receive</h3>
-            <p>Tutor recommendations within 24 hrs</p>
-          </li>
-          <li>
-            <span>Step 3</span>
-            <UserPlus width={30} height={30} />
-            <h3>Select</h3>
-            <p>A suitable tutor and start your first lesson</p>
-          </li>
-        </Con.List>
-      </Con>
-  )
+      <Con.Title> Request a Tutor via our simple 3-Step Process </Con.Title>
+      <Con.List>
+        <Con.ListItem>
+          <span>Step 1</span>
+          <div>
+            <ClipboardCheckmark width={30} height={30} color="#0E7BE5" />
+          </div>
+          <h3>Request</h3>
+          <p>For a suitable tutor Via a 3-min call</p>
+        </Con.ListItem>
+        <Con.ListItem>
+        <img src={Arrow1} alt="arrow" />
+        </Con.ListItem>
+        <Con.ListItem>
+          <span>Step 2</span>
+          <div>
+            <Like width={30} height={30} color="#0E7BE5" />
+          </div>
+          <h3>Receive</h3>
+          <p>Tutor recommendations within 24 hrs</p>
+        </Con.ListItem>
+        <Con.ListItem>
+          <img src={Arrow2} alt="arrow" />
+        </Con.ListItem>
+        <Con.ListItem>
+          <span>Step 3</span>
+          <div>
+            <UserPlus width={30} height={30} color="#0E7BE5" />
+          </div>
+          <h3>Select</h3>
+          <p>A suitable tutor and start your first lesson</p>
+        </Con.ListItem>
+      </Con.List>
+    </Con>
+  );
 }
